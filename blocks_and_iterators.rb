5bf8fc10a84084ee52996d9e93d78ac1ff52bb29
Javiera Tapia Bobadilla <areijav@Javieras-MacBook-Pro.local>
2021-01-30 22:30:40 -0300

    a tour of ruby

---
+++ blocks_and_iterators.rb
@@ -0,0 +1,35 @@
+# frozen_string_literal: true
+
+# Iterators are for "enumerables"
+# iterators => They behave like loops
+# block => body of the loop
+
+# For integers.
+puts "INTEGER SAMPLES"
+3.times { puts 'Ruby!' }
+1.upto(9) { |x| print x }
+
+# For arrays
+puts "\nARRAY SAMPLES"
+a = [3, 2, 1]
+a[3] = a[2] - 1
+a.each do |elt|
+  print elt + 1
+end
+
+# Other useful iterators
+a = [1, 2, 3, 4]
+a.map { |x| x * x } # square elems
+a.select(&:even?) # select even elements
+a.inject do |sum, x| # compute the sum of the elements => 1
+  sum + x
+end
+
+# For hashes (hashtable data structure)
+# hash associates arbitrary value objects with key objects.
+print "\nHASH SAMPLES"
+h = { one: 1, two: 2 }
+h[:three] = 3
+h.each do |key,value|
+  print "\n#{value}:#{key}"
+end
